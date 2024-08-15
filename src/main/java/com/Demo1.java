deb18296b2bebc833227770b2e8c2b042187d49d
ShubhamFase <shubhamfase2015@gmail.com>
2024-08-15 23:24:29 +0530

    This is my new Demo1.java
    "This is file from my remote"

---
+++ src/main/java/com/Demo1.java
@@ -0,0 +1,16 @@
+package com.practice;
+
+public class Demo1 extends Demo{
+
+	public static void main(String[] args) 
+	{
+		
+		Demo1 d1=new Demo1();
+		d1.sub1();
+	}
+    public void sub1() 
+    {
+    	System.out.println("hiiiiiiii");
+    	super.sub();
+    }
+}
